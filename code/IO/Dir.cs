1a0e4e81f1ce113cad846916e1180c73f969fa80
Gabriel-DC <gabrieldcgh@gmail.com>
2023-01-02 00:38:36 -0300

    Update Dir.cs

--- code/IO/Dir.cs
+++ code/IO/Dir.cs
@@ -7,10 +7,12 @@ namespace c_sharp_things.code.IO
 {
     public class Dir
     {
+        //LISTAR TODOS OS ARQUIVOS DE UMA PASTA E SUBPASTAS QUE TERMINAM COM .DLL
         public IEnumerable<string> FindFiles(string folderName)
         {
             List<string> salesFiles = new List<string>();
 
+            //LISTAR TODOS OS ARQUIVOS DE UMA PASTA E SUBPASTAS
             var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
 
             foreach (var file in foundFiles)
@@ -18,6 +20,22 @@ namespace c_sharp_things.code.IO
                     salesFiles.Add(file);
 
             return salesFiles;
+        }
+
+        //RETORNA O DIRETORIO ATUAL
+        public string CurrentDirectory()
+        {
+            //RETORNA O DIRETORIO ATUAL
+            var currentDirectory = Directory.GetCurrentDirectory();
+            return currentDirectory;
+        }
+
+        //RETORNA DIRETORIO ESPECIAL DOCUMENTOS
+        public string MyDocuments()
+        {
+            //RETORNA DIRETORIO ESPECIAL DOCUMENTOS
+            var myDocuments = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            return myDocuments;
         }
     }
 }
